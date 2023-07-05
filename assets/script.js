5842d0c125b0a792184723f9d39b3145be6cd20b
Stephane Nowicki <stephane.nowicki@yahoo.fr>
2023-07-05 10:37:16 +0200

    ajout de la condition pour le deplacement droite

--- assets/script.js
+++ assets/script.js
@@ -16,27 +16,27 @@ const slides = [
 		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	},
 ]
-console.log(slides[0])
+console.log(slides[0]) // afficher le nombre d'element dans le tableau //
 
-const arrowLeft = document.querySelector(".arrow_left")
+const arrowLeft = document.querySelector(".arrow_left") // appeler l'element html arrow_left //
 console.log(arrowLeft);
 
-const arrowRight = document.querySelector(".arrow_right")
+const arrowRight = document.querySelector(".arrow_right") // appeler l'element html arrow_right //
 console.log(arrowRight);
 
-const lengthSlides = slides.length
-console.log(slides.length);
+const lengthSlides = slides.length // variable est egale a la longueur du tableau //
+console.log(slides.length); // afficher la longueur du tableau //
 
-const dots = document.querySelector(".dots")
+const dots = document.querySelector(".dots") // appeler l'element html dots
 console.log(dots);
 
 function genererBullet() {
-	for (let i = 0; i < lengthSlides; i++) {
-		const dot = document.createElement("span");
-		dot.classList.add("dot")
-		dots.appendChild(dot)
+	for (let i = 0; i < lengthSlides; i++) { // condition (boucle) tant que i est inferieur a la longueur du tableau incrémenter de 1 //
+		const dot = document.createElement("span"); // creer un element html //
+		dot.classList.add("dot") // ajouter une nouvelle classe //
+		dots.appendChild(dot) // dot est l'enfant de dots //
 	}
-	dots.children[0].classList.add("dot_selected")
+	dots.children[0].classList.add("dot_selected") // ajouter la classe au premier enfant de dots //
 
 }
 genererBullet()
@@ -47,12 +47,15 @@ const tagLine = document.querySelector(".tagLine")
 let indexSlide = 0
 
 function deplacementDroite() {
-	indexSlide += 1
-	image.src = `./assets/images/slideshow/${slides[indexSlide].image}`
-	tagLine.innerHTML = slides[indexSlide].tagLine
-	dots.children[indexSlide].classList.add("dot_selected")
-	dots.children[indexSlide - 1].classList.remove("dot_selected")
-	
+	if (indexSlide === lengthSlides-1) { // si indexslide est egal a la longueur de mon tableau -1 alors indexslide revient a 0 //
+		indexSlide = -1;
+		dots.children[3].classList.remove("dot_selected");
+	}
+		indexSlide += 1;
+		image.src = `./assets/images/slideshow/${slides[indexSlide].image}`
+		tagLine.innerHTML = slides[indexSlide].tagLine // changer le contenu en html //
+		dots.children[indexSlide].classList.add("dot_selected")
+		dots.children[indexSlide -1].classList.remove("dot_selected");
 }
 
 function deplacementGauche() {
