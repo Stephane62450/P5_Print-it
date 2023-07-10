6d15aba4309d85870577571320d632aaf1fc2f3f
Stephane Nowicki <stephane.nowicki@yahoo.fr>
2023-07-10 11:02:17 +0200

    Modification de la ligne 52 et 63 pour rester dynamiquement en cas d'ajout ou de suppression d'un élément

--- assets/script.js
+++ assets/script.js
@@ -16,51 +16,51 @@ const slides = [
 		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	},
 ]
-console.log(slides[0]) // afficher le nombre d'element dans le tableau //
+console.log(slides[0]) // afficher le nombre d'element dans le tableau 
 
-const arrowLeft = document.querySelector(".arrow_left") // appeler l'element html arrow_left //
+const arrowLeft = document.querySelector(".arrow_left") // appeler l'element html arrow_left 
 console.log(arrowLeft);
 
-const arrowRight = document.querySelector(".arrow_right") // appeler l'element html arrow_right //
+const arrowRight = document.querySelector(".arrow_right") // appeler l'element html arrow_right 
 console.log(arrowRight);
 
-const lengthSlides = slides.length // variable est egale a la longueur du tableau //
+const lengthSlides = slides.length // variable est egale a la longueur du tableau
 console.log(slides.length); // afficher la longueur du tableau //
 
 const dots = document.querySelector(".dots") // appeler l'element html dots
 console.log(dots);
 
 function genererBullet() {
-	for (let i = 0; i < lengthSlides; i++) { // condition (boucle) tant que i est inferieur a la longueur du tableau incrémenter de 1 //
-		const dot = document.createElement("span"); // creer un element html //
-		dot.classList.add("dot") // ajouter une nouvelle classe //
-		dots.appendChild(dot) // dot est l'enfant de dots //
+	for (let i = 0; i < lengthSlides; i++) { // condition (boucle) tant que i est inferieur a la longueur du tableau incrémenter de 1
+		const dot = document.createElement("span"); // creer un element html
+		dot.classList.add("dot") // ajouter une nouvelle classe
+		dots.appendChild(dot) // dot est l'enfant de dots
 	}
-	dots.children[0].classList.add("dot_selected") // ajouter la classe au premier enfant de dots //
+	dots.children[0].classList.add("dot_selected") // ajouter la classe au premier enfant de dots
 
 }
 genererBullet()
 
-const image = document.querySelector(".banner-img")
+const image = document.querySelector(".banner-img") // appeler l'element html .banner-img
 const tagLine = document.querySelector(".tagLine")
-//const tagLine2 = document.querySelector("#banner p") = ciblage de l'element parent puis du p// 
-let indexSlide = 0
+//const tagLine2 = document.querySelector("#banner p") = ciblage de l'element parent puis du p 
+let indexSlide = 0 // attribuer la valeur 0
 
 function deplacementDroite() {
-	if (indexSlide === lengthSlides-1) { // si indexslide est egal a la longueur de mon tableau -1 alors indexslide revient a 0 //
-		indexSlide = -1;
-		dots.children[3].classList.remove("dot_selected");
+	if (indexSlide === lengthSlides-1) { // si indexslide est egal a la longueur de mon tableau -1 alors indexslide revient a 0
+		indexSlide = -1; // position de indexSlide
+		dots.children[lengthSlides-1].classList.remove("dot_selected"); // retirer la class au 3eme enfant 
 	}
 		indexSlide += 1;
 		image.src = `./assets/images/slideshow/${slides[indexSlide].image}`
 		tagLine.innerHTML = slides[indexSlide].tagLine // changer le contenu en html //
-		dots.children[indexSlide].classList.add("dot_selected")
-		dots.children[indexSlide -1].classList.remove("dot_selected");
+		dots.children[indexSlide].classList.add("dot_selected") // ajoute la classe à l'enfant de dots
+		dots.children[indexSlide -1].classList.remove("dot_selected"); // retire la classe à l'enfant de dots
 }
 
 function deplacementGauche() {
 	if (indexSlide === 0) {
-		indexSlide = +4;
+		indexSlide = lengthSlides;
 		dots.children[0].classList.remove("dot_selected");
 	}
 	indexSlide -= 1
